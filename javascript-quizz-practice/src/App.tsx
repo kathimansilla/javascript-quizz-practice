2ecc66573a969bd4d8e09a45aad42d7bdc0d62ad
kathimansilla <mansillakatherina@gmail.com>
2023-11-09 17:27:53 +0100

    init commit

---
+++ javascript-quizz-practice/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+import { Container, Stack, Typography } from '@mui/material';
+import { JavaScriptLogo } from './JavaScriptLogo';
+import { Start } from './Start';
+
+
+function App() {
+  return (
+    <main>
+      <Container maxWidth="sm">
+        <Stack
+          direction="row"
+          gap={2}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <JavaScriptLogo />
+          <Typography variant="h4" component="h1">
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+        <Start />
+      </Container>
+    </main>
+  );
+}
+
+export default App;
