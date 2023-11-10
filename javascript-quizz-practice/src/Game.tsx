dc84438f2911699d484d33cfe1ea05cd66409706
kathimansilla <mansillakatherina@gmail.com>
2023-11-10 08:32:37 +0100

    update state with user answer

--- javascript-quizz-practice/src/Game.tsx
+++ javascript-quizz-practice/src/Game.tsx
@@ -1,5 +1,5 @@
 //import { IconButton, Stack } from '@mui/material'
-import { Card, Typography } from '@mui/material';
+import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 import { useQuestionsStore } from './store/questions';
 import { type Question as QuestionType } from './types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
@@ -10,13 +10,23 @@ import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 //info es tipo info: QuestionType (types)
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant='outlined'>
-        <Typography variant='h6'>
+    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left', fontSize: 15, marginTop: 4}}>
+        <Typography variant='h7'>
             {info.question}
         </Typography>
         <SyntaxHighlighter language='javascript' style={gradientDark}>
             {info.code}
         </SyntaxHighlighter>
+
+        <List sx={{ bgcolor: '#333' }} disablePadding >
+            {info.answers.map((answer, index) => (
+                <ListItem key={index} disablePadding divider >
+                    <ListItemButton >
+                        <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
     </Card>
   )
 };
--- javascript-quizz-practice/src/store/questions.ts
+++ javascript-quizz-practice/src/store/questions.ts
@@ -1,22 +1,36 @@
-import { create } from 'zustand'
-import { type Question } from '../types'
+import { create } from 'zustand';
+import { type Question } from '../types';
 
 interface State {
-    questions: Question[]
-    currentQuestion: number
-    fetchQuestions: (limit: number) => Promise<void>
+  questions: Question[];
+  currentQuestion: number;
+  fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
-    return {
-        questions: [],
-        currentQuestion: 0,
-        fetchQuestions: async (limit: number) => {
-            const res = await fetch('http://localhost:5173/data.json')
-            const json = await res.json()
-            const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
-            set({ questions })
-            
-    }
-    }
-})
+  return {
+    questions: [],
+    currentQuestion: 0,
+    fetchQuestions: async (limit: number) => {
+      const res = await fetch('http://localhost:5173/data.json');
+      const json = await res.json();
+      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
+      set({ questions });
+    },
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      const { questions } = get();
+      const newQuestions = structuredClone(questions);
+      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+      const questionInfo = newQuestions[questionIndex];
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+      //cambiar esta información en la copia de la pregunta
+      (newQuestions[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex,
+      }),
+        set({ questions: newQuestions });
+    },
+  };
+});
