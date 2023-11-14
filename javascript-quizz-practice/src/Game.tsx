a57a4bdb2d4fe982a82155fd38b0e398bf5a5807
kathimansilla <mansillakatherina@gmail.com>
2023-11-14 15:43:58 +0100

    Add pagination

--- javascript-quizz-practice/src/Game.tsx
+++ javascript-quizz-practice/src/Game.tsx
@@ -1,14 +1,33 @@
 //import { IconButton, Stack } from '@mui/material'
-import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from '@mui/material';
 import { useQuestionsStore } from './store/questions';
 import { type Question as QuestionType } from './types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material';
 
-
+//Si pusiera esta función dentro, se renderizaría cada vez
+//También así es más fácil hacerle testing
+const getBackGroundColor = (info: QuestionType, index: number) => {
+  const { correctAnswer, userSelectedAnswer } = info
+  if (userSelectedAnswer == null) return 'transparent'
+  //answer incorrect and no selected by user
+  if (index !== correctAnswer && index !== userSelectedAnswer ) return 'transparent'
+  if (index === correctAnswer) return 'green'
+  if (index === userSelectedAnswer) return 'red'
+  //si no es ninguna de las anteriores
+  return 'transparent'
+}
 
 //info es tipo info: QuestionType (types)
 const Question = ({ info }: { info: QuestionType }) => {
+
+    const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+    const createHandleClick = (answerIndex: number) => () => {
+        selectAnswer(info.id, answerIndex )
+    }
+
   return (
     <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left', fontSize: 15, marginTop: 4}}>
         <Typography variant='h7'>
@@ -21,11 +40,11 @@ const Question = ({ info }: { info: QuestionType }) => {
         <List sx={{ bgcolor: '#333' }} disablePadding >
             {info.answers.map((answer, index) => (
                 <ListItem key={index} disablePadding divider >
-                    <ListItemButton >
+                    <ListItemButton onClick={createHandleClick(index)} sx={{ backgroundColor: getBackGroundColor(info, index) }} disabled={info.userSelectedAnswer != null} >
                         <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
                     </ListItemButton>
                 </ListItem>
-            ))}
+            ))} 
         </List>
     </Card>
   )
@@ -34,10 +53,23 @@ const Question = ({ info }: { info: QuestionType }) => {
 export const Game = () => {
   const questions = useQuestionsStore((state) => state.questions);
   const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
+  const goNextQuestion = useQuestionsStore((state) => state.goNextQuestion);
+  const goPrevQuestion = useQuestionsStore((state) => state.goPrevQuestion);
 
   const questionInfo = questions[currentQuestion];
   return (
     <>
+    <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+      <IconButton onClick={goPrevQuestion} disabled={currentQuestion === 0}>
+        <ArrowBackIosNew />
+      </IconButton>
+
+    {currentQuestion + 1} / {questions.length}
+
+      <IconButton onClick={goNextQuestion} disabled={currentQuestion >= questions.length - 1}>
+        <ArrowForwardIos />
+      </IconButton>
+    </Stack>
       <Question info={questionInfo} />
     </>
   );
--- javascript-quizz-practice/src/store/questions.ts
+++ javascript-quizz-practice/src/store/questions.ts
@@ -1,36 +1,62 @@
 import { create } from 'zustand';
 import { type Question } from '../types';
+import confetti from 'canvas-confetti'
 
 interface State {
   questions: Question[];
   currentQuestion: number;
   fetchQuestions: (limit: number) => Promise<void>;
   selectAnswer: (questionId: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPrevQuestion: () => void;
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
   return {
     questions: [],
     currentQuestion: 0,
+    
     fetchQuestions: async (limit: number) => {
       const res = await fetch('http://localhost:5173/data.json');
       const json = await res.json();
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
       set({ questions });
     },
+
     selectAnswer: (questionId: number, answerIndex: number) => {
       const { questions } = get();
+      // structuredClone para una actualización profunda del state
       const newQuestions = structuredClone(questions);
       const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
       const questionInfo = newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex; //boolean
+      if (isCorrectUserAnswer) confetti()
       //cambiar esta información en la copia de la pregunta
-      (newQuestions[questionIndex] = {
+      newQuestions[questionIndex] = {
         ...questionInfo,
         isCorrectUserAnswer,
         userSelectedAnswer: answerIndex,
-      }),
+      },
         set({ questions: newQuestions });
     },
+
+    goNextQuestion: () => {
+      const { currentQuestion, questions } = get()
+      const nextQuestion = currentQuestion + 1
+
+      if (nextQuestion < questions.length) {
+        set({ currentQuestion: nextQuestion  })
+      }
+    },
+
+    goPrevQuestion: () => {
+      const { currentQuestion } = get()
+      const previousQuestion = currentQuestion - 1
+
+      if (previousQuestion >= 0) {
+        set({ currentQuestion: previousQuestion  })
+      }
+    }
+
   };
 });
