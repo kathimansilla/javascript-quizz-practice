03810fce38c3fa0313aa98bfbfbf7c5b2a77daa9
kathimansilla <mansillakatherina@gmail.com>
2023-11-14 17:26:27 +0100

    build page

--- javascript-quizz-practice/vite.config.ts
+++ javascript-quizz-practice/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kathimansilla.github.io/javascript-quizz-practice/'
+  base: 'https://kathimansilla.github.io/javascript-quizz-practice'
 })
